db5974373cbc87af27c564de409f4ee5af72985c
Miaoqi Chu mac <mqichu@anl.gov>
2022-02-01 22:26:47 -0600

    compute q partition with qrings

--- pysimplemask/simple_mask_kernel.py
+++ pysimplemask/simple_mask_kernel.py
@@ -61,6 +61,7 @@ class SimpleMask(object):
         self.extent = None
         self.hdl.scene.sigMouseMoved.connect(self.show_location)
         self.bad_pixel_set = set()
+        self.qrings = []
 
         self.idx_map = {
             0: "scattering",
@@ -108,6 +109,8 @@ class SimpleMask(object):
         mask = self.mask_kernel.get_one_mask(target)
         self.mask_kernel.enable(target)
         self.mask = np.logical_and(self.mask, mask)
+        if target == "mask_qring":
+            self.qrings = self.mask_kernel.workers[target].get_qrings()
         self.data[1:] *= self.mask
         if self.plot_log:
             self.data[1][np.logical_not(self.mask)] = self.saxs_log_min
@@ -245,6 +248,8 @@ class SimpleMask(object):
 
         self.shape = self.data_raw[0].shape
 
+        # reset the qrings after data loading
+        self.qrings = []
         self.qmap = self.compute_qmap()
         self.mask_kernel = MaskAssemble(self.shape, self.saxs_log,
                                         self.qmap['q'])
@@ -476,7 +481,8 @@ class SimpleMask(object):
     def remove_roi(self, roi):
         self.hdl.remove_item(roi)
 
-    def get_partition(self, mask=None, num=400, style='linear', mode='q'):
+    def get_partition(self, mask=None, num=400, style='linear', mode='q',
+                      qrings=None):
         """
         get_partion computes the partition for either qmap or phi-map
         Args:
@@ -485,6 +491,8 @@ class SimpleMask(object):
             num: integer, number of points
             style = ['linear', 'logarithmic']. logarithmic only works for q map
             mode = ['q', 'phi']
+            qrings: list of tuples (qmin, qmax) or empty list or None, describe
+                the start and end q value
         """
         assert mode in ['q', 'phi']
         if mask is None:
@@ -492,22 +500,43 @@ class SimpleMask(object):
 
         vmap = self.qmap[mode] * mask
         vmap_valid = vmap[self.mask == 1]
-        vmin = np.min(vmap_valid)
-        vmax = np.max(vmap_valid)
+        if qrings is None or len(qrings) == 0:
+            vmin = np.min(vmap_valid)
+            vmax = np.max(vmap_valid)
+            qrings = [(vmin, vmax)]
 
+        qspan = []
+        qlist = []
+
+        num_rings = len(qrings)
+        pnum = num // num_rings     # partial number in each ring 
         if mode == 'phi' or style == 'linear':
-            qspan = np.linspace(vmin, vmax, num + 1)
-            qlist = (qspan[1:] + qspan[:-1]) / 2.0
+            for m in range(num_rings):
+                vmin, vmax = qrings[m]
+                tmp = np.linspace(vmin, vmax, pnum + 1)
+                qspan.append(tmp)
+                qlist.append((tmp[1:] + tmp[:-1]) / 2.0)
         elif style == 'logarithmic':
-            qmin = np.log10(vmin)
-            qmax = np.log10(vmax)
-            qspan = np.logspace(qmin, qmax, num + 1)
-            qlist = np.sqrt(qspan[1:] * qspan[:-1])
+            for m in range(num_rings):
+                vmin, vmax = qrings[m]
+                qmin = np.log10(vmin)
+                qmax = np.log10(vmax)
+                tmp = np.logspace(qmin, qmax, pnum + 1)
+                qspan.append(tmp)
+                qlist.append(np.sqrt(tmp[1:] * tmp[:-1]))
 
         partition = np.zeros_like(vmap, dtype=np.uint32)
-        for n in range(num):
-            val = qspan[n]
-            partition[vmap >= val] = n + 1
+        for m in range(num_rings):
+            for n in range(pnum):
+                val = qspan[m][n]
+                partition[vmap >= val] = n + 1 + (pnum * m)  # overall offset
+        qlist = np.hstack(qlist)
+        # remove one element starting from the second array so that the 
+        # dimension matches
+        for n in range(1, num_rings):
+            qspan[n] = qspan[n][1:]
+        qspan = np.hstack(qspan)
+ 
         qspan = qspan.reshape(1, -1)
         return qspan, qlist, partition
 
@@ -567,14 +596,22 @@ class SimpleMask(object):
         if self.meta is None or self.data_raw is None:
             return
 
+        # make the dq number equal to the multiples of num-rings
+        num_rings = len(self.qrings)
+        if num_rings > 0:
+            dq_num = (dq_num + num_rings - 1) // num_rings * num_rings
+
         # make the static values multiples of the dynamic value
         sq_num = (sq_num + dq_num - 1) // dq_num * dq_num
         sp_num = (sp_num + dp_num - 1) // dp_num * dp_num
 
         dqspan, dqval_1d, dqmap_partition = \
-            self.get_partition(num=dq_num, style=style, mode='q')
+            self.get_partition(num=dq_num, style=style, mode='q',
+                               qrings=self.qrings)
+
         sqspan, sqval_1d, sqmap_partition = \
-            self.get_partition(num=sq_num, style=style, mode='q')
+            self.get_partition(num=sq_num, style=style, mode='q',
+                               qrings=self.qrings)
 
         dphispan, dphival_1d, dphi_partition = \
             self.get_partition(num=dp_num, style=style, mode='phi')
@@ -600,8 +637,8 @@ class SimpleMask(object):
             'staticMap': sta_map,
             'dphival': dphival,
             'sphival': sphival,
-            'dynamicQList': np.arange(0, dq_num + 1).reshape(1, -1),
-            'staticQList': np.arange(0, sq_num + 1).reshape(1, -1),
+            'dynamicQList': np.arange(0, dq_num * dp_num + 1).reshape(1, -1),
+            'staticQList': np.arange(0, sq_num * sp_num + 1).reshape(1, -1),
             'dqspan': dqspan,
             'sqspan': sqspan,
             'dphispan': dphispan,
