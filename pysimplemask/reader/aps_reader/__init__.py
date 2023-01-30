5e702d2bf4d0c452d5ef2ae929910a36f5a6f7a1
Miaoqi Chu win <mqichu@anl.gov>
2023-01-30 16:38:37 -0600

    add esrf support

--- pysimplemask/reader/aps_reader/__init__.py
+++ pysimplemask/reader/aps_reader/__init__.py
@@ -1,5 +1,6 @@
 from .hdf_handler import HdfDataset
 from .imm_handler import ImmDataset
 from .rigaku_handler import RigakuDataset
+from .esrf_hdf_handler import EsrfHdfDataset
 
 __all__ = (HdfDataset, ImmDataset, RigakuDataset)
