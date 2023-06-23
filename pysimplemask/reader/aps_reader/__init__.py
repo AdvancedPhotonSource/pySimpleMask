29b060ecca8335e93299bed4e5cf41c4740efb2a
Miaoqi Chu <mqichu@anl.gov>
2023-06-22 23:44:21 -0500

    add rigaku six module

--- pysimplemask/reader/aps_reader/__init__.py
+++ pysimplemask/reader/aps_reader/__init__.py
@@ -1,6 +1,7 @@
 from .hdf_handler import HdfDataset
 from .imm_handler import ImmDataset
 from .rigaku_handler import RigakuDataset
+from .rigaku_six_handler import RigakuSixDataset
 from .esrf_hdf_handler import EsrfHdfDataset
 
-__all__ = (HdfDataset, ImmDataset, RigakuDataset)
+__all__ = (HdfDataset, ImmDataset, RigakuDataset, RigakuSixDataset, EsrfHdfDataset)
