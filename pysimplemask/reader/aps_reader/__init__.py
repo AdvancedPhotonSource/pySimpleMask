54b71734fea5ae5ad9550874885ac6afa4a20081
mqichu <mqichu@anl.gov>
2023-12-21 00:02:59 -0600

    implement rigaku 3M

--- pysimplemask/reader/aps_reader/__init__.py
+++ pysimplemask/reader/aps_reader/__init__.py
@@ -1,7 +1,7 @@
 from .hdf_handler import HdfDataset
 from .imm_handler import ImmDataset
 from .rigaku_handler import RigakuDataset
-from .rigaku_six_handler import RigakuSixDataset
+from .rigaku_3M_handler import Rigaku3MDataset
 from .esrf_hdf_handler import EsrfHdfDataset
 
-__all__ = (HdfDataset, ImmDataset, RigakuDataset, RigakuSixDataset, EsrfHdfDataset)
+__all__ = (HdfDataset, ImmDataset, RigakuDataset, Rigaku3MDataset, EsrfHdfDataset)
