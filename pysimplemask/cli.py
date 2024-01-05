a0677fb0ce0a64dfcb2f3dbd11ea3a24528e0e91
mqichu <mqichu@anl.gov>
2024-01-05 02:32:39 -0600

    use current working directory

--- pysimplemask/cli.py
+++ pysimplemask/cli.py
@@ -1,12 +1,13 @@
 """Console script for pysimplemask."""
 import argparse
 import sys
+import os
 
 
 def main():
     """Console script for pysimplemask."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', nargs='?')
+    parser.add_argument('path', default=os.getcwd())
     args = parser.parse_args()
     kwargs = vars(args)
     from .mask_gui import run
