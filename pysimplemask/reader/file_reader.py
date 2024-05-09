d2ce80441c73fba55f5ad7627b6d03a091d78b03
Miaoqi Chu <mqichu@anl.gov>
2024-05-09 13:22:00 -0500

    add function to reader class

--- pysimplemask/reader/file_reader.py
+++ pysimplemask/reader/file_reader.py
@@ -13,7 +13,38 @@ class FileReader(object):
     def __init__(self, fname) -> None:
         self.fname = fname
         self.ftype = 'Base Class'
+        self.saxs = None
+        self.meta = None
+        self.saxs_lin_min = None 
+        self.saxs_log_min = None
+        self.saxs_lin = None 
+        self.saxs_log = None
         pass
+
+    def clean_data(self, mask=None):
+        mask = self.saxs > 0
+        saxs_nonzero_1d = self.saxs[mask]
+        self.saxs_lin_min = np.percentile(saxs_nonzero_1d, 1)
+        self.saxs_log_min = np.log10(self.saxs_lin_min)
+        self.saxs_lin = np.copy(self.saxs)
+        # self.saxs_lin[~mask] += self.saxs_lin_min
+        self.saxs_lin += self.saxs_lin_min
+        self.saxs_log = np.log10(self.saxs_lin)
+        # self.saxs_log[~mask] = np.nan
+    
+    def get_center(self):
+        center = (self.meta['bcx'], self.meta['bcy'])
+        return center
+    
+    def get_scattering_with_mask(self, mask, log_style=True):
+        roi = mask > 0
+        if log_style:
+            data = np.copy(self.saxs_log)
+        else:
+            data = np.copy(self.saxs_lin)
+        data_min = np.min(data[roi])
+        data[~roi] = data_min
+        return data
 
     def get_scattering(self, *kargs, **kwargs):
         raise NotImplementedError
