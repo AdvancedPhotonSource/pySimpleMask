73292498ecfd72a9bcdb7f11cbdf06c133e1336b
Miaoqi Chu <mqichu@anl.gov>
2025-03-13 02:18:08 -0500

    update fake data

--- src/pysimplemask/file_reader.py
+++ src/pysimplemask/file_reader.py
@@ -40,9 +40,9 @@ def get_fake_metadata(shape):
     logger.warning('failed to get the raw metadata, using default values instead')
     metadata = {
         # 'datetime': "2022-05-08 14:00:51,799",
-        'energy': 12.4,         # keV
-        'det_dist': 11500,       # mm
-        'pix_dim': 75e-3,       # mm
+        'energy': 12.3,         # keV
+        'det_dist': 12.3456,      # meter 
+        'pix_dim': 75e-3,       # meter
         'bcx': 773.0,
         'bcy': 809.0
     }
