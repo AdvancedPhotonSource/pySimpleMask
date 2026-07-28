cdce9b341caa71f3e2f0cc7145f92a22ead5905f
Miaoqi Chu <mqichu@anl.gov>
2026-07-28 01:05:33 -0500

    fix(gui): set left panel groupboxes and MaskWidget to Preferred horizontal size
    The Input, Mask, Partition, Output groupboxes and the MaskWidget QTabWidget
    had hsizetype="Minimum", which caused the horizontal splitter to treat each
    panel's natural content width as a hard floor, making the left column hard to
    shrink. Changing to "Preferred" lets the splitter honour the drag position
    at any width.
    
    Also regenerates ui_mask.py from the updated mask.ui.

--- src/pysimplemask/gui/view/ui_mask.py
+++ src/pysimplemask/gui/view/ui_mask.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'mask.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.8.3
+## Created by: Qt User Interface Compiler version 6.6.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -52,7 +52,7 @@ class Ui_SimpleMask(object):
         self.splitter.setOrientation(Qt.Orientation.Vertical)
         self.groupBox = QGroupBox(self.splitter)
         self.groupBox.setObjectName(u"groupBox")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy1.setHorizontalStretch(1)
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
@@ -203,7 +203,7 @@ class Ui_SimpleMask(object):
         self.splitter.addWidget(self.groupBox)
         self.groupBox_2 = QGroupBox(self.splitter)
         self.groupBox_2.setObjectName(u"groupBox_2")
-        sizePolicy5 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        sizePolicy5 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy5.setHorizontalStretch(1)
         sizePolicy5.setVerticalStretch(1)
         sizePolicy5.setHeightForWidth(self.groupBox_2.sizePolicy().hasHeightForWidth())
@@ -244,11 +244,8 @@ class Ui_SimpleMask(object):
 
         self.MaskWidget = QTabWidget(self.groupBox_2)
         self.MaskWidget.setObjectName(u"MaskWidget")
-        sizePolicy6 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
-        sizePolicy6.setHorizontalStretch(0)
-        sizePolicy6.setVerticalStretch(0)
-        sizePolicy6.setHeightForWidth(self.MaskWidget.sizePolicy().hasHeightForWidth())
-        self.MaskWidget.setSizePolicy(sizePolicy6)
+        sizePolicy.setHeightForWidth(self.MaskWidget.sizePolicy().hasHeightForWidth())
+        self.MaskWidget.setSizePolicy(sizePolicy)
         self.MaskWidget.setMaximumSize(QSize(16777215, 16777215))
         self.tab_6 = QWidget()
         self.tab_6.setObjectName(u"tab_6")
@@ -268,18 +265,21 @@ class Ui_SimpleMask(object):
         self.gridLayout_12.setObjectName(u"gridLayout_12")
         self.label_18 = QLabel(self.groupBox_8)
         self.label_18.setObjectName(u"label_18")
-        sizePolicy7 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
-        sizePolicy7.setHorizontalStretch(0)
-        sizePolicy7.setVerticalStretch(0)
-        sizePolicy7.setHeightForWidth(self.label_18.sizePolicy().hasHeightForWidth())
-        self.label_18.setSizePolicy(sizePolicy7)
+        sizePolicy6 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
+        sizePolicy6.setHorizontalStretch(0)
+        sizePolicy6.setVerticalStretch(0)
+        sizePolicy6.setHeightForWidth(self.label_18.sizePolicy().hasHeightForWidth())
+        self.label_18.setSizePolicy(sizePolicy6)
 
         self.gridLayout_12.addWidget(self.label_18, 0, 0, 1, 1)
 
         self.label_32 = QLabel(self.groupBox_8)
         self.label_32.setObjectName(u"label_32")
-        sizePolicy6.setHeightForWidth(self.label_32.sizePolicy().hasHeightForWidth())
-        self.label_32.setSizePolicy(sizePolicy6)
+        sizePolicy7 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        sizePolicy7.setHorizontalStretch(0)
+        sizePolicy7.setVerticalStretch(0)
+        sizePolicy7.setHeightForWidth(self.label_32.sizePolicy().hasHeightForWidth())
+        self.label_32.setSizePolicy(sizePolicy7)
 
         self.gridLayout_12.addWidget(self.label_32, 1, 0, 1, 1)
 
@@ -323,15 +323,15 @@ class Ui_SimpleMask(object):
         self.gridLayout_8.setObjectName(u"gridLayout_8")
         self.label_14 = QLabel(self.tab_2)
         self.label_14.setObjectName(u"label_14")
-        sizePolicy6.setHeightForWidth(self.label_14.sizePolicy().hasHeightForWidth())
-        self.label_14.setSizePolicy(sizePolicy6)
+        sizePolicy7.setHeightForWidth(self.label_14.sizePolicy().hasHeightForWidth())
+        self.label_14.setSizePolicy(sizePolicy7)
 
         self.gridLayout_8.addWidget(self.label_14, 1, 0, 1, 1)
 
         self.label_8 = QLabel(self.tab_2)
         self.label_8.setObjectName(u"label_8")
-        sizePolicy6.setHeightForWidth(self.label_8.sizePolicy().hasHeightForWidth())
-        self.label_8.setSizePolicy(sizePolicy6)
+        sizePolicy7.setHeightForWidth(self.label_8.sizePolicy().hasHeightForWidth())
+        self.label_8.setSizePolicy(sizePolicy7)
 
         self.gridLayout_8.addWidget(self.label_8, 1, 2, 1, 1)
 
@@ -477,15 +477,15 @@ class Ui_SimpleMask(object):
 
         self.label_25 = QLabel(self.groupBox_9)
         self.label_25.setObjectName(u"label_25")
-        sizePolicy7.setHeightForWidth(self.label_25.sizePolicy().hasHeightForWidth())
-        self.label_25.setSizePolicy(sizePolicy7)
+        sizePolicy6.setHeightForWidth(self.label_25.sizePolicy().hasHeightForWidth())
+        self.label_25.setSizePolicy(sizePolicy6)
 
         self.gridLayout_15.addWidget(self.label_25, 0, 5, 1, 1)
 
         self.label_33 = QLabel(self.groupBox_9)
         self.label_33.setObjectName(u"label_33")
-        sizePolicy7.setHeightForWidth(self.label_33.sizePolicy().hasHeightForWidth())
-        self.label_33.setSizePolicy(sizePolicy7)
+        sizePolicy6.setHeightForWidth(self.label_33.sizePolicy().hasHeightForWidth())
+        self.label_33.setSizePolicy(sizePolicy6)
 
         self.gridLayout_15.addWidget(self.label_33, 1, 5, 1, 1)
 
@@ -754,8 +754,8 @@ class Ui_SimpleMask(object):
 
         self.label_2 = QLabel(self.tab6)
         self.label_2.setObjectName(u"label_2")
-        sizePolicy7.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
-        self.label_2.setSizePolicy(sizePolicy7)
+        sizePolicy6.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy6)
 
         self.gridLayout_28.addWidget(self.label_2, 3, 0, 1, 1)
 
@@ -856,7 +856,7 @@ class Ui_SimpleMask(object):
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.groupBox_3 = QGroupBox(self.layoutWidget)
         self.groupBox_3.setObjectName(u"groupBox_3")
-        sizePolicy16 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        sizePolicy16 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
         sizePolicy16.setHorizontalStretch(2)
         sizePolicy16.setVerticalStretch(1)
         sizePolicy16.setHeightForWidth(self.groupBox_3.sizePolicy().hasHeightForWidth())
@@ -1202,7 +1202,7 @@ class Ui_SimpleMask(object):
 
         self.groupBox_6 = QGroupBox(self.layoutWidget)
         self.groupBox_6.setObjectName(u"groupBox_6")
-        sizePolicy17 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+        sizePolicy17 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
         sizePolicy17.setHorizontalStretch(1)
         sizePolicy17.setVerticalStretch(0)
         sizePolicy17.setHeightForWidth(self.groupBox_6.sizePolicy().hasHeightForWidth())
@@ -1217,8 +1217,8 @@ class Ui_SimpleMask(object):
 
         self.label_47 = QLabel(self.groupBox_6)
         self.label_47.setObjectName(u"label_47")
-        sizePolicy7.setHeightForWidth(self.label_47.sizePolicy().hasHeightForWidth())
-        self.label_47.setSizePolicy(sizePolicy7)
+        sizePolicy6.setHeightForWidth(self.label_47.sizePolicy().hasHeightForWidth())
+        self.label_47.setSizePolicy(sizePolicy6)
 
         self.gridLayout_7.addWidget(self.label_47, 0, 1, 1, 1)
 
@@ -1260,8 +1260,8 @@ class Ui_SimpleMask(object):
 
         self.label_11 = QLabel(self.groupBox_5)
         self.label_11.setObjectName(u"label_11")
-        sizePolicy7.setHeightForWidth(self.label_11.sizePolicy().hasHeightForWidth())
-        self.label_11.setSizePolicy(sizePolicy7)
+        sizePolicy6.setHeightForWidth(self.label_11.sizePolicy().hasHeightForWidth())
+        self.label_11.setSizePolicy(sizePolicy6)
 
         self.gridLayout_16.addWidget(self.label_11, 0, 0, 1, 1)
 
@@ -1302,8 +1302,8 @@ class Ui_SimpleMask(object):
 
         self.line_3 = QFrame(self.groupBox_4)
         self.line_3.setObjectName(u"line_3")
-        self.line_3.setFrameShape(QFrame.Shape.VLine)
-        self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
+        self.line_3.setFrameShape(QFrame.VLine)
+        self.line_3.setFrameShadow(QFrame.Sunken)
 
         self.gridLayout_3.addWidget(self.line_3, 1, 6, 1, 1)
 
@@ -1320,8 +1320,8 @@ class Ui_SimpleMask(object):
 
         self.label_frame = QLabel(self.groupBox_4)
         self.label_frame.setObjectName(u"label_frame")
-        sizePolicy6.setHeightForWidth(self.label_frame.sizePolicy().hasHeightForWidth())
-        self.label_frame.setSizePolicy(sizePolicy6)
+        sizePolicy7.setHeightForWidth(self.label_frame.sizePolicy().hasHeightForWidth())
+        self.label_frame.setSizePolicy(sizePolicy7)
 
         self.gridLayout_3.addWidget(self.label_frame, 0, 0, 1, 1)
 
@@ -1363,22 +1363,22 @@ class Ui_SimpleMask(object):
 
         self.line_2 = QFrame(self.groupBox_4)
         self.line_2.setObjectName(u"line_2")
-        self.line_2.setFrameShape(QFrame.Shape.VLine)
-        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
+        self.line_2.setFrameShape(QFrame.VLine)
+        self.line_2.setFrameShadow(QFrame.Sunken)
 
         self.gridLayout_3.addWidget(self.line_2, 1, 10, 1, 1)
 
         self.line = QFrame(self.groupBox_4)
         self.line.setObjectName(u"line")
-        self.line.setFrameShape(QFrame.Shape.VLine)
-        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+        self.line.setFrameShape(QFrame.VLine)
+        self.line.setFrameShadow(QFrame.Sunken)
 
         self.gridLayout_3.addWidget(self.line, 0, 8, 1, 1)
 
         self.label_6 = QLabel(self.groupBox_4)
         self.label_6.setObjectName(u"label_6")
-        sizePolicy6.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
-        self.label_6.setSizePolicy(sizePolicy6)
+        sizePolicy7.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
+        self.label_6.setSizePolicy(sizePolicy7)
 
         self.gridLayout_3.addWidget(self.label_6, 1, 0, 1, 1)
 
@@ -1406,8 +1406,8 @@ class Ui_SimpleMask(object):
 
         self.line_4 = QFrame(self.groupBox_4)
         self.line_4.setObjectName(u"line_4")
-        self.line_4.setFrameShape(QFrame.Shape.VLine)
-        self.line_4.setFrameShadow(QFrame.Shadow.Sunken)
+        self.line_4.setFrameShape(QFrame.VLine)
+        self.line_4.setFrameShadow(QFrame.Sunken)
 
         self.gridLayout_3.addWidget(self.line_4, 1, 2, 1, 1)
 
